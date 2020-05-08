54dad7f6a417ceb318e5051b2b7f9fd7d42cd662
Richard Barrington-Hill <bazzlad@gmail.com>
2020-05-08 20:59:09 +0200

    Update header.php

--- public/templates/components/header.php
+++ public/templates/components/header.php
@@ -8,6 +8,8 @@
 	<meta name="description" content="">
 	<meta name="keywords" content="">
 
+	<meta name='apple-mobile-web-app-capable' content='yes' />
+	<meta name='apple-mobile-web-app-status-bar-style' content='black' />
 
 	<meta name="version" content="<?php echo $GLOBALS['version']; ?>">
 
